da0e121052d29ad1cd579fec0760bef0bc9bf5ad
Alexis Zeballos <zeballosalexis0@gmail.com>
2022-11-06 13:20:55 -0300

    first commit

---
+++ app/components/gui_app.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+
+def menu_bar(root):
+    menu_bar = tk.Menu(root)
+    root.config(menu = menu_bar, width=300, height=300)
+    
+    main_menu = tk.Menu(menu_bar, tearoff=0)
+    menu_bar.add_cascade(Label = 'Inicio', menu = main_menu)
+
+    main_menu.add_command(Label = 'Crear registro DB')
+    main_menu.add_command(Label = 'Eliminar registro DB')
+    main_menu.add_command(Label = 'Salir', command = root.destroy)
+
+    menu_bar.add_cascade(menu = 'Consultas')
+    menu_bar.add_cascade(menu = 'Configuracion')
+    menu_bar.add_cascade(menu = 'Ayuda')
+
+    '''
+    para agregar comandos a la cascada de de cada menu se debe utilizar el comando .add_command()
+
+    ej: main_menu.add_command(Label = 'lo que se queira utilizar')
+    '''
+
+class Frame(tk.Frame):
+    def __init__(self, root = None):
+        super().__init__(root, width=480, height=300)
+        self.root = root
+        self.pack()
+
+        self.elem_field()
+
+    def elem_field(self):
+         self.name_label = tk.Label(self, text = '''nombre de lo que ira en la base de datos''')
+         self.name_label.config(font = ('Arial', 12, 'bold'))
+         self.name_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+
+         self.sec_name_label = tk.Label(self, text = '''nombre de lo que ira en la base de datos''')
+         self.sec_name_label.config(font = ('Arial', 12, 'bold'))
+         self.sec_name_label.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+         self.third_name_label = tk.Label(self, text = '''nombre de lo que ira en la base de datos''')
+         self.third_name_label.config(font = ('Arial', 12, 'bold'))
+         self.third_name_label.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+         self.entry_name = tk.Entry(self)
+         self.entry_name.config(width = 50, state = 'disabled', font = ('Arial', 12))
+         self.entry_name.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+         self.sec_entry_name = tk.Entry(self)
+         self.sec_entry_name.config(width = 50, state = 'disabled', font = ('Arial', 12))
+         self.sec_entry_name.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+         self.third_entry_name = tk.Entry(self)
+         self.third_entry_name.config(width = 50, state = 'disabled', font = ('Arial', 12))
+         self.third_entry_name.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+
---
+++ app/main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from client.gui_app import Frame
+
+def main():
+    root = tk.Tk() # esta clase generara la interfaz de la aplicacion
+
+    root.title('[+] inserte titulo de aplicacion') # modifica el titlulo de la aplicacion
+    root.iconbitmap(''' aqui se debe especificar la ruta del icono, el mismo se alojara en la carpeta assets''') # metodo capaz de invocar el incono de la aplicacion
+    # el siguiente metodo permitira o negara la odificacion del tamaño de la ventana
+    # los parametros otorgados a la funcion indicaran si se puede o no modificar lo mismo
+    # en caso de ser .resizable(0, 0) no se podra modificar el tamaño de la app, en caso de ser (1, 0) se podra modificar solo hacia los lados
+    # en caso de ser (0, 1), solo podra modificarse de arriba a abajo, y en caso de ser (1, 1) se podra modificar en todas direcciones
+    root.resizable(1, 1)
+
+    app = Frame(root = root)
+
+    root.mainloop() # esto permitira que la aplicacion se mantega corriendo
+
+if __name__ == '__main__':
+    main()
