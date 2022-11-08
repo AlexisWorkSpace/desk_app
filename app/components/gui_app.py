81ed2ca5b94a632998c4281977f0d87b754ec210
Alexis Zeballos <97553981+AlexisWorkSpace@users.noreply.github.com>
2022-11-08 15:00:05 -0300

    Add files via upload

--- app/components/gui_app.py
+++ app/components/gui_app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def menu_bar(root):
     menu_bar = tk.Menu(root)
@@ -28,6 +29,8 @@ class Frame(tk.Frame):
         self.pack()
 
         self.elem_field()
+        self.disabled_fields()
+        self.data_tab()
 
     def elem_field(self):
          self.name_label = tk.Label(self, text = '''nombre de lo que ira en la base de datos''')
@@ -37,7 +40,6 @@ class Frame(tk.Frame):
                  column = 0, 
                  padx = 10, 
                  pady = 10)
-
 
          self.sec_name_label = tk.Label(self, text = '''nombre de lo que ira en la base de datos''')
          self.sec_name_label.config(font = ('Arial', 12, 'bold'))
@@ -55,7 +57,8 @@ class Frame(tk.Frame):
                  padx = 10, 
                  pady = 10)
 
-         self.entry_name = tk.Entry(self)
+         self.name = tk.StringVar()
+         self.entry_name = tk.Entry(self, textvariable = self.name)
          self.entry_name.config(
                  width = 50,
                  font = ('Arial', 12))
@@ -66,7 +69,8 @@ class Frame(tk.Frame):
                  pady = 10,
                  columnspan = 2)
 
-         self.sec_entry_name = tk.Entry(self)
+         self.sec_name = tk.StringVar()
+         self.sec_entry_name = tk.Entry(self, textvariable = self.sec_name)
          self.sec_entry_name.config(
                  width = 50, 
                  font = ('Arial', 12))
@@ -77,7 +81,8 @@ class Frame(tk.Frame):
                  pady = 10,
                  columnspan = 2)
 
-         self.third_entry_name = tk.Entry(self)
+         self.third_name = tk.StringVar()
+         self.third_entry_name = tk.Entry(self, textvariable = self.third_name)
          self.third_entry_name.config(
                  width = 50, 
                  font = ('Arial', 12))
@@ -88,7 +93,7 @@ class Frame(tk.Frame):
                  pady = 10,
                  columnspan = 2)
 
-         self.new_button = tk.Button(self, text = 'Nuevo elemento', self.enable_fields)
+         self.new_button = tk.Button(self, text = 'Nuevo elemento', command = self.enable_fields)
          self.new_button.config(
                  width = 20, 
                  font = ('Arial', 12, 'bold'), 
@@ -97,55 +102,77 @@ class Frame(tk.Frame):
                  cursor = 'hand2'
                  activebackground = '#35BD6F')
          self.new_button.grid(
-                 row = 4, 
+                 row = 3, 
                  column = 0, 
                  padx = 10, 
                  pady =10)
 
-         self.delete_button = tk.Button(self, text = 'borrar elemento')
-         self.delete_button.config(
+         self.save_button = tk.Button(self, text = 'guardar elemento', command = self.data_upload)
+         self.save_button.config(
                  width = 20, 
                  font = ('Arial', 12, 'bold'), 
                  fg = '#FFFFFF', 
                  bg = '#FF0000', 
                  cursor = 'hand2'
                  activebackground = '#3586DF')
-         self.delete_button.grid(
-                 row = 4, 
+         self.save_button.grid(
+                 row = 3, 
                  column = 1, 
                  padx = 10, 
                  pady =10)
 
-         self.cancel_button = tk.Button(self, text = 'Cancelar')
+         self.cancel_button = tk.Button(self, text = 'Cancelar', command = self.disabled_fields)
          self.cancel_button.config(
-                 width = 20, 
+                  width = 20, 
                  font = ('Arial', 12, 'bold'), 
                  fg = '#FFFFFF', 
                  bg = '#0000FF', 
                  cursor = 'hand2'
                  activebackground = '#E15370')
          self.cancel_button.grid(
-                 row = 4, 
+                 row = 3, 
                  column = 2, 
                  padx = 10, 
                  pady =10)
 
-        def enable_fields(self):
-            self.entry_name.config(state = 'normal')
-            self.sec_entry_name.config(state = 'normal')
-            self.third_entry_name.config(state = 'normal')
+    def enable_fields(self):
+        self.entry_name.config(state = 'normal')
+        self.sec_entry_name.config(state = 'normal')
+        self.third_entry_name.config(state = 'normal')
 
-            self.delete_button.config(state = 'normal')
-            self.cancel_button.config(state = 'normal')
+        self.delete_button.config(state = 'normal')
+        self.cancel_button.config(state = 'normal')
 
   
 
-        def disabled_fields(self):
-            self.entry_name.config(state = 'disabled')
-            self.sec_entry_name.config(state = 'disabled')
-            self.third_entry_name.config(state = 'disabled')
+    def disabled_fields(self):
+        self.name.set('')
+        self.sec_name.set('')
+        self.third_name.set('')
 
-            self.delete_button.config(state = 'disabled')
-            self.cancel_button.config(state = 'disabled')
+        self.entry_name.config(state = 'disabled')
+        self.sec_entry_name.config(state = 'disabled')
+        self.third_entry_name.config(state = 'disabled')
+
+        self.delete_button.config(state = 'disabled')
+        self.cancel_button.config(state = 'disabled')
+
+    def data_upload(self):
+
+        self.disabled_fields()
+
+    def data_tab(self):
+        self.table = ttk.Treeview(
+                self, 
+                column = ('name', 'sec_name', 'third_name'))
+        self.table.grid(row = 4, column = 0, columnspan = 4)
+        
+        self.table.heading('#0', text = 'ID')
+        self.table.heading('#1', text = 'name')
+        self.table.heading('#2', text = 'sec_name')
+        self.table.heading('#3', text = 'third_name')
+
+        self.tabla.insert('', 0, text = '1', 
+                values = ('algo', 'otra cosa', 'otra mas'))
 
 
